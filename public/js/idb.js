92ff058930a491191a307f8c458e458d49a18c06
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-03-04 13:47:29 -0500

    added event listener

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,3 +2,11 @@
 let db;
 // Establish a connection to IndexedDB database called 'budget'. Set it to version 1.
 const request = indexedDB.open('budget', 1);
+
+// This event will emit if the db version changes.
+request.onupgradeneeded = function(event) {
+    // Save a reference to the db
+    const db = event.target.result;
+    // Create an object store (table) called 'new_transaction', set it to have an auto increment primary key.
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};

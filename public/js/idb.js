194c61962eb42e3978b4f03511ad95664a723913
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-03-04 13:39:32 -0500

    establish connection in idb js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,1 +1,4 @@
-// IndexedDB file
+// Create variable to hold db connection
+let db;
+// Establish a connection to IndexedDB database called 'budget'. Set it to version 1.
+const request = indexedDB.open('budget', 1);
